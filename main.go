6a08f41047fe757010d4a2bc537c82617307dd21
Nima Naghibi <dude.nimanaghibi@gmail.com>
2026-06-25 23:11:18 +0330

    feat: update the cosumeloop for a streaming server mode

--- main.go
+++ main.go
@@ -10,6 +10,11 @@ import (
 	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
 )
 
+type Storer interface {
+	Put(string, []byte) error
+	Get(string) ([]byte, error)
+}
+
 type Storage struct {
 	mu   sync.RWMutex
 	data map[string][]byte
@@ -21,12 +26,24 @@ func NewStorage() *Storage {
 	}
 }
 
-func (s *Storage) put(key string, val []byte) error {
+func (s *Storage) Put(key string, val []byte) error {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 	s.data[key] = val
 
 	return nil
+}
+
+func (s *Storage) Get(key string) ([]byte, error) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	val, ok := s.data[key]
+
+	if !ok {
+		return nil, fmt.Errorf("value not found")
+	}
+
+	return val, nil
 }
 
 var (
@@ -47,10 +64,14 @@ type Message struct {
 
 func main() {
 	produce()
-	consume()
 }
 
-func consume() {
+type Consumer struct {
+	consumer *kafka.Consumer
+	Storage  Storer
+}
+
+func NewConsumer(storage Storer) (*Consumer, error) {
 	c, err := kafka.NewConsumer(&kafka.ConfigMap{
 		"bootstrap.servers":        "localhost:9093",
 		"broker.address.family":    "v4",
@@ -61,19 +82,25 @@ func consume() {
 	})
 
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
-
-	fmt.Printf("Created consumer %v\n", c)
 
 	err = c.SubscribeTopics([]string{topic}, nil)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 
+	return &Consumer{
+		Storage:  NewStorage(),
+		consumer: c,
+	}, nil
+}
+
+// it's a streaming server so it's considered a loop in my opinion
+func (c *Consumer) consumeLoop() {
 	for {
 		// poll(), poll the consumer for msgs and events
-		ev := c.Poll(100)
+		ev := c.consumer.Poll(100)
 		if ev == nil {
 			continue
 		}
@@ -81,7 +108,7 @@ func consume() {
 		// e -> kafka event
 		switch e := ev.(type) {
 		case *kafka.Message:
-			_, err := c.StoreMessage(e)
+			_, err := c.consumer.StoreMessage(e)
 			if err != nil {
 				fmt.Println("store message error: ", err)
 			}
